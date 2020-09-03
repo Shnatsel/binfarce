3e65fd0593777f2e33110b6cb6c7237004d641f4
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-09-03 23:12:52 +0200

    Change ar parser public interface to return errors and handle skip() errors

--- src/ar.rs
+++ src/ar.rs
@@ -4,25 +4,25 @@ use crate::ByteOrder;
 use crate::parser::*;
 use crate::demangle::SymbolName;
 
-pub fn parse(data: &[u8]) -> Vec<String> {
+pub fn parse(data: &[u8]) -> Result<Vec<String>, UnexpectedEof> {
     const MAGIC: &[u8] = b"!<arch>\x0A";
 
     if data.get(0..8) != Some(MAGIC) {
-        return Vec::new();
+        return Ok(Vec::new());
     }
 
     let mut s = Stream::new(&data[8..], ByteOrder::BigEndian);
     while !s.at_end() {
         // Align the offset.
         if s.offset() & 1 == 1 {
-            s.skip_len(1);
+            s.skip_len(1)?;
         }
 
         let identifier = str::from_utf8(s.read_bytes(16)).unwrap();
-        s.skip_len(12); // timestamp
-        s.skip_len(6); // owner_id
-        s.skip_len(6); // group_id
-        s.skip_len(8); // mode
+        s.skip_len(12)?; // timestamp
+        s.skip_len(6)?; // owner_id
+        s.skip_len(6)?; // group_id
+        s.skip_len(8)?; // mode
         let file_size = str::from_utf8(s.read_bytes(10)).unwrap();
         let terminator = s.read_bytes(2);
 
@@ -44,16 +44,16 @@ pub fn parse(data: &[u8]) -> Vec<String> {
 
         if name.is_empty() && identifier == "/               " {
             let index_data = s.read_bytes(file_size);
-            return parse_sysv(index_data);
+            return Ok(parse_sysv(index_data));
         } else if name == "__.SYMDEF" {
             let index_data = s.read_bytes(file_size);
             return parse_bsd(index_data);
         } else {
-            s.skip_len(file_size);
+            s.skip_len(file_size)?;
         }
     }
 
-    Vec::new()
+    Ok(Vec::new())
 }
 
 fn parse_sysv(data: &[u8]) -> Vec<String> {
@@ -84,12 +84,12 @@ fn parse_sysv(data: &[u8]) -> Vec<String> {
     symbols
 }
 
-fn parse_bsd(data: &[u8]) -> Vec<String> {
+fn parse_bsd(data: &[u8]) -> Result<Vec<String>, UnexpectedEof> {
     let mut symbols = Vec::new();
 
     let mut s = Stream::new(data, ByteOrder::LittleEndian);
     let entries_len = s.read::<u32>() as usize;
-    s.skip_len(entries_len);
+    s.skip_len(entries_len)?;
     let strings_len = s.read::<u32>() as usize;
     let strings = s.read_bytes(strings_len);
 
@@ -104,5 +104,5 @@ fn parse_bsd(data: &[u8]) -> Vec<String> {
         }
     }
 
-    symbols
+    Ok(symbols)
 }
