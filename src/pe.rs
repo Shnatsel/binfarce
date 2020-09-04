d2815e94ac626f150c43b8fc915b1b40f52dac93
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2020-09-05 01:58:57 +0200

    Rewrite PE parser to be zero-allocation

--- src/pe.rs
+++ src/pe.rs
@@ -46,8 +46,8 @@ impl Section <'_> {
 #[derive(Debug, Clone)]
 pub struct Pe<'a> {
     data: &'a [u8],
+    pe_pointer: usize,
     header: PeHeader,
-    sections: Vec<Section<'a>>,
 }
 
 fn parse_pe_header(s: &mut Stream) -> Result<PeHeader, UnexpectedEof> {
@@ -70,39 +70,10 @@ pub fn parse(data: &[u8]) -> Result<Pe, ParseError> {
     let mut s = Stream::new_at(data, pe_pointer, ByteOrder::LittleEndian)?;
     let header = parse_pe_header(&mut s)?;
 
-    let sections_offset = //TODO: harden
-          pe_pointer
-        + SIZEOF_PE_MAGIC
-        + SIZEOF_COFF_HEADER
-        + header.size_of_optional_header as usize;
-
-    // Don't preallocate space for more than 1024 entries; it's rare in the wild and may OOM
-    let mut sections = Vec::with_capacity(header.number_of_sections.into());
-    let mut s = Stream::new_at(data, sections_offset, ByteOrder::LittleEndian)?;
-    for i in 0..header.number_of_sections {
-        let name = s.read_bytes(8)?;
-        let virtual_size: u32 = s.read()?;
-        s.skip::<u32>()?; // virtual_address
-        let size_of_raw_data: u32 = s.read()?;
-        let pointer_to_raw_data: u32 = s.read()?;
-        s.skip_len(16)?; // other data
-
-        let len = name.iter().position(|c| *c == 0).unwrap_or(8);
-        // ignore sections with non-UTF8 names since the spec says they must be UTF-8
-        if let Ok(name_str) = std::str::from_utf8(&name[0..len]) {
-            sections.push(Section{
-                name: name_str,
-                virtual_size,
-                size_of_raw_data,
-                pointer_to_raw_data,
-                index: i.into(),
-            })
-        }
-    }
     Ok(Pe {
         data,
+        pe_pointer,
         header,
-        sections,
     })
 }
 
@@ -111,21 +82,53 @@ impl Pe<'_> {
         self.header
     }
 
-    pub fn sections(&self) -> Vec<Section> {
-        self.sections.clone()
+    pub fn section_with_name(&self, name: &str) -> Result<Option<Section>, ParseError> {
+        let callback = |section: Section| {
+            section.name == name
+        };
+        self.find_section(callback)
     }
 
-    pub fn section_with_name(&self, section_name: &str) -> Option<Section> {
-        self.sections.iter().find(|x| {
-            x.name == section_name
-        }).cloned()
+    pub fn find_section<F: Fn(Section) -> bool>(&self, callback: F) -> Result<Option<Section>, ParseError> {
+        let sections_offset = //TODO: harden
+            self.pe_pointer
+            + SIZEOF_PE_MAGIC
+            + SIZEOF_COFF_HEADER
+            + self.header.size_of_optional_header as usize;
+
+        let mut s = Stream::new_at(self.data, sections_offset, ByteOrder::LittleEndian)?;
+        for i in 0..self.header.number_of_sections {
+            let name = s.read_bytes(8)?;
+            let virtual_size: u32 = s.read()?;
+            s.skip::<u32>()?; // virtual_address
+            let size_of_raw_data: u32 = s.read()?;
+            let pointer_to_raw_data: u32 = s.read()?;
+            s.skip_len(16)?; // other data
+
+            let len = name.iter().position(|c| *c == 0).unwrap_or(8);
+            // ignore sections with non-UTF8 names since the spec says they must be UTF-8
+            if let Ok(name_str) = std::str::from_utf8(&name[0..len]) {
+                let section = Section {
+                    name: name_str,
+                    virtual_size,
+                    size_of_raw_data,
+                    pointer_to_raw_data,
+                    index: i.into(),
+                };
+                if callback(section) {
+                    return Ok(Some(section));
+                }
+            }
+        }
+        Ok(None)
     }
 
     pub fn symbols(&self) -> Result<(Vec<SymbolData>, u64), ParseError> {
         let number_of_symbols = self.header.number_of_symbols as usize;
         let mut symbols = Vec::with_capacity(number_of_symbols);
 
-        let text_section = self.section_with_name(".text").unwrap();
+        let text_section = self.section_with_name(".text")?
+            .ok_or(ParseError::MalformedInput)?;
         let text_section_size = text_section.size_of_raw_data;
         let text_section_index = text_section.index;
     
